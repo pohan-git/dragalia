e8ce9459a1c869ee70fb246186f24799a78c0c81
李柏漢 <brian780405@gmail.com>
2019-09-30 19:59:24 +0800

    Updates

---
+++ precache-manifest.8684716a9dc008e457e23591119fa78b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcbc152d2c7bdac9f7787cde69620911",
+    "url": "/dragalia/index.html"
+  },
+  {
+    "revision": "a19e48fb4afb45d14e08",
+    "url": "/dragalia/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "198121acd64216d2cc6c",
+    "url": "/dragalia/static/js/2.81d79ca4.chunk.js"
+  },
+  {
+    "revision": "a19e48fb4afb45d14e08",
+    "url": "/dragalia/static/js/main.f76f3691.chunk.js"
+  },
+  {
+    "revision": "4b5eba60403e37fd8033",
+    "url": "/dragalia/static/js/runtime-main.718223ec.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/dragalia/static/media/logo.25bf045c.svg"
+  }
+]);
